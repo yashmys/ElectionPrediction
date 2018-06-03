01cf7fa3d5564b6af4c50e1923d8485dd4594877
yash <yash.mys@gmail.com>
2018-06-03 00:59:09 -0400

    commit

--- Elections.R
+++ Elections.R
@@ -1,8 +1,10 @@
 install.packages("mice")
+install.packages("contrib.url")
 library('tidyverse')
 library('caTools')
 library('ROCR')
 library('mice')
+
 
 polling <- read.csv("PollingData.csv")
 dim(polling)
@@ -21,3 +23,34 @@ summary(imputed)
 polling$Rasmussen <-  imputed$Rasmussen
 polling$SurveyUSA <-  imputed$SurveyUSA
 summary(polling)
+
+Train <- subset(polling, polling$Year == 2004 | polling$Year == 2008)
+Test <- subset(polling,polling$Year == 2012)
+dim(Train)
+
+# get the baseline accuracy
+table(Train$Republican,sign(Train$Rasmussen))
+
+#get the corelation among the independent variables 
+cor(Train[c("Rasmussen","SurveyUSA","DiffCount","PropR","Republican")])
+
+mod1 <- glm(Train$Republican ~ Train$PropR,data = Train,family = binomial)
+summary(mod1)
+
+pred1 <- predict(mod1,type="response")
+summary(pred1)
+pred1
+
+table(Train$Republican,pred1 > 0.5)
+
+mod2 <-  glm(Republican ~ SurveyUSA + DiffCount,data = Train,family = binomial)
+summary(mod2)
+pred2 <- predict(mod2,type="response")
+table(Train$Republican,pred2 > 0.5)
+
+
+# baseline model for test sample
+dim(Test)
+table(Test$Republican,sign(Test$Rasmussen))
+TestPrediction <- predict(mod2,newdata = Test,type = "response")
+table(Test$Republican,TestPrediction > 0.5)
