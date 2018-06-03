3e1f93a3b04674d8341d3bff7ab0003ab3a4a88b
yash <yash.mys@gmail.com>
2018-06-02 22:31:55 -0400

    commit

--- Elections.R
+++ Elections.R
@@ -1,3 +1,23 @@
+install.packages("mice")
 library('tidyverse')
 library('caTools')
 library('ROCR')
+library('mice')
+
+polling <- read.csv("PollingData.csv")
+dim(polling)
+names(polling)
+table(polling$Year)
+summary(polling)
+
+req_col <- subset(polling,select = c ( "Rasmussen" , "SurveyUSA" , "DiffCount" , "PropR" ))
+dim(req_col)
+head(req_col)
+
+set.seed(300)
+imputed <-  complete(mice(req_col))
+summary(imputed)
+
+polling$Rasmussen <-  imputed$Rasmussen
+polling$SurveyUSA <-  imputed$SurveyUSA
+summary(polling)
